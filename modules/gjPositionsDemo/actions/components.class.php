8a22352c650b4fa8409f18c2b641036ca75d99c8
caefer <caefer@ical.ly>
2010-11-08 14:43:07 +0100

    * added gjPositionsDemo module with the two components and three partials configured in the app.yml

---
+++ modules/gjPositionsDemo/actions/components.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/BasegjPositionsDemoComponents.class.php';
+
+/**
+ * gjPositionsDemo components.
+ * 
+ * @package    gjPositionsDemoPlugin
+ * @subpackage gjPositionsDemo
+ * @author     caefer
+ * @version    SVN: $Id: components.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+class gjPositionsDemoComponents extends BasegjPositionsDemoComponents
+{
+}
---
+++ modules/gjPositionsDemo/lib/BasegjPositionsDemoComponents.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Base components for the gjPositionsDemoPlugin gjPositionsDemo module.
+ * 
+ * @package     gjPositionsDemoPlugin
+ * @subpackage  gjPositionsDemo
+ * @author      caefer
+ * @version     SVN: $Id: BaseComponents.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+abstract class BasegjPositionsDemoComponents extends sfComponents
+{
+  public function executeSlideshow(sfWebRequest $request)
+  {
+  }
+
+  public function executeRelatedbykeywords(sfWebRequest $request)
+  {
+  }
+}
