c94b0f0ab7b8ede1537a19d631770696ab75c128
caefer <caefer@ical.ly>
2010-11-11 12:02:44 +0100

    * added descriptive texts to app.yml and made block elements from manuallink and slideshow

--- modules/gjPositionsDemo/templates/_manuallink.php
+++ modules/gjPositionsDemo/templates/_manuallink.php
@@ -1,1 +1,3 @@
-<a href="<?php echo $params['url'] ?>" title="<?php echo $params['title'] ?>"><?php echo $params['text'] ?></a>
+<div class="design_element manuallink">
+  <a href="<?php echo $params['url'] ?>" title="<?php echo $params['title'] ?>"><?php echo $params['text'] ?></a>
+</div>
--- modules/gjPositionsDemo/templates/_slideshow.php
+++ modules/gjPositionsDemo/templates/_slideshow.php
@@ -1,1 +1,3 @@
+<div class="design_element slideshow">
 slideshow
+</div>
