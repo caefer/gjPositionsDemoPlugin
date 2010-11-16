ee8bdfa36f5aea68452d916e6e83d8040854d46a
caefer <caefer@ical.ly>
2010-11-16 16:01:56 +0100

    * implemented how to access assigned contents

--- modules/gjPositionsDemo/templates/_latestArticles.php
+++ modules/gjPositionsDemo/templates/_latestArticles.php
@@ -1,10 +1,19 @@
 <?php use_helper('Text') ?>
 <div class="design_element latestArticles">
+<h3>Latest articles</h3>
 <ul>
 <?php foreach($articles as $i => $article): ?>
   <li>
     <h3><?php echo $article['headline'] ?></h3>
     <p><?php echo truncate_text($article['text'], 100) ?></p>
+  </li>
+<?php endforeach; ?>
+</ul>
+<h3>Manually assigned contents</h3>
+<ul>
+<?php foreach($contents as $i => $content): ?>
+  <li>
+    <h3><?php echo $content ?></h3>
   </li>
 <?php endforeach; ?>
 </ul>
