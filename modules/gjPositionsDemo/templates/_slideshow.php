a73c9adc844d4b47ba40c6e61cdce26044010e57
caefer <caefer@ical.ly>
2011-01-27 14:40:48 +0100

    * now sues the new override feature of gjPositionsPlugin

--- modules/gjPositionsDemo/templates/_slideshow.php
+++ modules/gjPositionsDemo/templates/_slideshow.php
@@ -1,3 +1,10 @@
 <div class="design_element slideshow">
 slideshow
+<ul>
+<?php foreach($contents as $i => $content): ?>
+  <li>
+    <h3><?php echo $content['override']['title'] ? $content['override']['title'] : $content['Object']['title'] ?></h3>
+  </li>
+<?php endforeach; ?>
+</ul>
 </div>
